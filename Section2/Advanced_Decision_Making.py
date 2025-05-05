e8c6d46f096272195514529172a5618584b39f58
Aleksey Myakinin <amykis@mail.ru>
2025-05-05 17:21:32 +0300

    Membership Checks

--- Section2/Advanced_Decision_Making.py
+++ Section2/Advanced_Decision_Making.py
@@ -1,12 +1,18 @@
 '''
-Challenge Easy
-Create a program that takes an integer score from the user. The program should use the ternary operator to determine the status based on the score:
+You are given a list of names and a dictionary of student grades. Write a program that:
 
-If the score is greater than or equal to 50, the status should be "Pass".
-If the score is less than 50, the status should be "Fail".
-The program should output the determined status.
+Checks if the name "Alice" is in the list.
+Checks if the name "David" is not in the list.
+Checks if the key "Bob" exists in the dictionary.
+Checks if the key "Eve" does not exist in the dictionary.
 '''
 
-takes = int(input())
-status = "Pass" if takes >= 50 else "Fail"
-print(status)
+# Given data
+names = ["Alice", "Bob", "Charlie"]
+grades = {"Alice": 85, "Bob": 90, "Charlie": 78}
+
+# Write code here
+print('Alice is in the list.') if "Alice" in names else None
+print('David is not in the list.') if "David" not in names else None
+print('Bob is in the dictionary.') if "Bob" in grades else None
+print('Eve is not in the dictionary.') if "Eve" not in grades else None
