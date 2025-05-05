86fc11f2d7216ced76dd71c123c9f1ca3bc450f8
Aleksey Myakinin <amykis@mail.ru>
2025-05-05 15:26:46 +0300

    Introduction

---
+++ Section1/Introduction/Introduction.py
@@ -0,0 +1,9 @@
+'''The Language
+Python is one of the world's easiest and most popular programming languages.
+
+Challenge
+Beginner
+Press the run code button to run your first code in Python
+'''
+
+print("Hello Python!")
