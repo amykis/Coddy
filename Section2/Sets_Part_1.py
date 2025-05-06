3a9ab1aee2893fd3fede876d86ab61cdeefc8c80
Aleksey Myakinin <amykis@mail.ru>
2025-05-06 09:25:19 +0300

    Recap - Remove Duplicates

--- Section2/Sets_Part_1.py
+++ Section2/Sets_Part_1.py
@@ -1,21 +1,20 @@
 '''
-Write a program that performs the following tasks:
+Write a function named remove_duplicates that takes a list of numbers as an argument
+and returns a new list with duplicates removed while preserving the original order of elements.
 
-Create a set called numbers containing the values 10, 20, 30, 40, 50.
-Use the discard() method to remove 35 from the set.
-Use the pop() method to remove an arbitrary element and store it in a variable called popped_element.
-Use the clear() method to empty the set.
-Print the following:
-The set after using discard().
-The value of popped_element.
-The set after using clear().
+Example Input:
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+Example Output:
+
+[1, 2, 3, 4, 5]
+Use the properties of sets to simplify your solution while keeping the order intact!
 '''
 
-# Write code here
-numbers = {10, 20, 30, 40, 50}
-numbers.discard(35)
-print(numbers)
-popped_element = numbers.pop()
-print(popped_element)
-numbers.clear()
-print(numbers)
+def remove_duplicates(numbers):
+    # Write code here
+    new_numbers_list = []
+    my_set = set(numbers)
+    for el in my_set:
+        new_numbers_list.append(el)
+    return new_numbers_list
