ab468dc624007bb595709b26168618d026daf8d3
Aleksey Myakinin <amykis@mail.ru>
2025-05-08 21:23:47 +0300

    Student Records Manager task2

--- Section2/Student_Records_Manager.py
+++ Section2/Student_Records_Manager.py
@@ -10,12 +10,32 @@ This project demonstrates the practical use of dictionaries for structured data,
 and advanced decision-making for efficient data management. It’s a practical way to apply Python concepts in a real-world scenario.
 
 
-Challenge
+Challenge task2
 
 Easy
-Initialize an empty dictionary named student_records to store the details of all students.
-This dictionary will serve as the foundation for the project, where each student's name will be a key,
+Create a function named add_student that takes three arguments: name (string), age (integer), and courses (a list of strings). The function should:
+
+Check if the student name already exists in the student_records dictionary. If it does, print "Student '<name>' already exists.".
+If the name does not exist, add it to student_records with age, an empty set for grades, and a set of courses.
+Print "Student '<name>' added successfully.".
+Add the following block of code at the bottom of your code:
+
+add_student("Alice", 20, ["Math", "Physics"])
+add_student("Bob", 22, ["Biology", "Chemistry"])
+print(student_records)l be a key,
 and their details (age, grades, and courses) will be stored as a nested dictionary.
 '''
 
+
+def add_student(name, age, courses):
+    if name in student_records:
+        print(f"Student '{name}' already exists.")
+    else:
+        student_records[name] = {"age": age, "grades": set(), "courses": courses}
+        print(f"Student '{name}' added successfully.")
+
+
 student_records = {}
+add_student("Alice", 20, ["Math", "Physics"])
+add_student("Bob", 22, ["Biology", "Chemistry"])
+print(student_records)
