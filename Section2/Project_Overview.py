16d753ef92adc094a4fbc7a5e12d8173addf092e
Aleksey Myakinin <amykis@mail.ru>
2025-05-12 15:01:43 +0300

    Project Overview task4

--- Section2/Project_Overview.py
+++ Section2/Project_Overview.py
@@ -6,27 +6,22 @@ updating stock, checking availability, and generating sales reports.
 It integrates dictionaries for structured data, error handling for invalid inputs,
 and logic operations for decision-making, making it a comprehensive and practical project.
 
-Challenge task3
+Challenge task4
 
-Easy
-Create a function named update_stock that takes two arguments: item (string) and quantity (int). The function should:
+Create a function named check_availability that takes one argument: item (string). The function should:
 
 Check if the item exists in the inventory dictionary.
-If it does not exist, print "Error: Item '<item>' not found.".
-If the item exists, calculate the new stock by adding the quantity to the current stock.
-If the new stock is negative, print "Error: Insufficient stock for '<item>'." and do not update.
-Otherwise, update the stock in the inventory.
-Print "Stock for '<item>' updated successfully.".
-
+If it does not exist, return "Item not found".
+If the item exists, return the current stock of the item.
 Add (replace) the following block of code at the bottom of your code:
+
 add_item("Apple", 0.5, 100)
 add_item("Banana", 0.2, 50)
-add_item("Apple", 0.6, 30)  # Should print an error
 update_stock("Apple", -20)
 update_stock("Banana", 30)
-update_stock("Orange", 10)  # Should print an error
-update_stock("Apple", -90)
-print(inventory)
+print(check_availability("Apple"))  # Should return 80
+print(check_availability("Banana"))  # Should return 80
+print(check_availability("Orange"))  # Should return "
 '''
 
 def add_item(item, price, stock):
@@ -47,26 +42,30 @@ def update_stock(item, quantity):
         print(f"Stock for '{item}' updated successfully.")
 
 
+def check_availability(item):
+    if item in inventory:
+        return inventory[item]["stock"]
+    else:
+        return "Item not found"
+
 
 inventory = {}
 
 add_item("Apple", 0.5, 100)
 add_item("Banana", 0.2, 50)
-add_item("Apple", 0.6, 30)  # Should print an error
 update_stock("Apple", -20)
 update_stock("Banana", 30)
-update_stock("Orange", 10)  # Should print an error
-update_stock("Apple", -90)
-print(inventory)
+print(check_availability("Apple"))  # Should return 80
+print(check_availability("Banana"))  # Should return 80
+print(check_availability("Orange"))  # Should return "
 
 
 '''
 Item 'Apple' added successfully.
 Item 'Banana' added successfully.
-Error: Item 'Apple' already exists.
 Stock for 'Apple' updated successfully.
 Stock for 'Banana' updated successfully.
-Error: Item 'Orange' not found.
-Error: Insufficient stock for 'Apple'.
-{'Apple': {'price': 0.5, 'stock': 80}, 'Banana': {'price': 0.2, 'stock': 80}}
+80
+80
+Item not found
 '''
