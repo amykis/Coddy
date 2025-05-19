acca94377807f68e0291c4d7f204bfc770384148
Aleksey Myakinin <amykis@mail.ru>
2025-05-19 09:20:42 +0300

    Higher-Order Functions task4

--- Section2/Higher-Order_Functions.py
+++ Section2/Higher-Order_Functions.py
@@ -1,19 +1,16 @@
 """
-Challenge task3
+Challenge task4
 
-Create a function named get_long_strings that takes a list of strings strings as an argument.
-The function should use the filter() function along with a lambda function to select strings
-that have a length greater than 5. The function should return a list containing the selected strings.
+Create a function named get_prime_numbers that takes a list of integers numbers as an argument.
+The function should use the filter() function along with a lambda function to select prime numbers from the list.
+A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+The function should return a list containing the selected prime numbers.
 """
 
 
-def get_long_strings(strings):
-    # Use filter() with a lambda function to select strings with length greater than 5
-    long_strings = filter(lambda word: len(word) > 5, strings)
+def get_prime_numbers(numbers):
+    return list(filter(lambda n: n > 1 and all(n % i != 0 for i in range(2, int(n**0.5) + 1)), numbers))
 
-    # Return the list of selected strings
-    return list(long_strings)
+numbers = [23, 29, 31, 32, 33, 37, 39, 41, 42, 43]
+print(get_prime_numbers(numbers))
 
-
-string = ["apple", "banana", "cherry", "dragonfruit"]
-print(get_long_strings(string))
